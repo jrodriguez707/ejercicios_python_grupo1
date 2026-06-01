f5accec4332e6484f3a5844edd1371dce401ae16
ccarjim2909 <158286303+ccarjim2909@users.noreply.github.com>
2026-06-01 11:34:01 +0200

    Ejercicio 1 terminado

---
+++ modulo4/ejercicio01.py
@@ -0,0 +1,7 @@
+# Definimos una función llamada saludar
+def saludar():
+    # Mostramos el mensaje solicitado
+    print("Bienvenido al programa de cálculo en Python")
+
+# Llamamos a la función desde el programa principal
+saludar()
