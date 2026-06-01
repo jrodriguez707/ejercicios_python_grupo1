5f8647269976f30dc76085d2d6e8e3d1b8924b8b
jrodriguez707 <jrodriguezr@iesrafaelalberti.es>
2026-06-01 10:48:38 +0200

    ejercicio1 modulo 1

---
+++ modulo1/ejercicio1.py
@@ -0,0 +1,9 @@
+edad = 22
+altura = 1.75
+nombre = "Jesús"
+estudiante = True
+
+print(edad, type(edad))
+print(altura, type(altura))
+print(nombre, type(nombre))
+print(estudiante, type(estudiante))
