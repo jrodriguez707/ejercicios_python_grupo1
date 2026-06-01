c48ace0edce1b9afac37a5d7f58f2d50ef439c09
jrodriguez707 <jrodriguezr@iesrafaelalberti.es>
2026-06-01 11:12:21 +0200

    ejercicio 6 modulo1

---
+++ modulo1/ejercicio6.py
@@ -0,0 +1,9 @@
+# Ejercicio 6 - Entrada y salida formateada
+
+# Solicitar datos al usuario
+nombre = input("Introduce tu nombre: ")
+carrera = input("Introduce la carrera que estudias: ")
+anio = input("Introduce el año de estudio: ")
+
+# Mostrar mensaje formateado
+print(f"\nHola, mi nombre es {nombre}, estudio {carrera} y estoy en el año {anio}.")
